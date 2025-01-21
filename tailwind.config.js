59bd28f6b1afcca20571fee8163aa14fa6ddcd58
carthik7989 <cojective.karthik@gmail.com>
2025-01-21 18:14:53 +0530

    Fix alignment issues and update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./docs/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    'hidden',
+    'bg-slate-400',
+    'bg-gray-200',
+    'bg-gray-400',
+    'bg-white',
+    'hover:bg-gray-100',
+    'hover:cursor-pointer',
+    'w-1/2',
+    'h-1/2',
+    'z-10',
+    'absolute'
+  ]
 }
